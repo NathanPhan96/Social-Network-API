4410c3927fecd5a3f8677e9cb6e4c8faae063687
Nathan Phan <nathanphan96@yahoo.com>
2023-03-18 21:06:02 -0700

    updated code for thought routes

--- models/Thought.js
+++ models/Thought.js
@@ -18,10 +18,10 @@ const thoughtSchema = new Schema(
       type: String,
       required: true
     },
-    reactions: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Reaction',
-    }],
+    reactions: [
+      reactionSchema
+      ],
+      
   },
   {
     toJSON: {
